181e14ead63740fcb5b97ff419b1da105a086159
Marc <marc.a.pardee@gmail.com>
2023-07-04 18:43:46 -0600

    Guest users added

--- gameshow/views.py
+++ gameshow/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from random import randint
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
 def room(request, room_name):
-    return render(request, "room.html", {"room_name": room_name, "user": request.GET.get('username')})
+    if request.GET.get('username') != None:
+        user = request.GET.get('username')
+    else:
+        user = "Guest{}".format(randint(1,1000))
+    return render(request, "room.html", {"room_name": room_name, "user": user})
 
 def host(request, room_name):
     return render(request, "host.html", {"room_name": room_name})
